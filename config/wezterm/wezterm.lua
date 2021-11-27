b552a6fe3d9780bc242f4b0aa773a926edc1412b
Kyle Rassweiler <kylerassweiler@gmail.com>
2021-11-27 11:03:17 -0500

    Add wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm';
+return {
+	font = wezterm.font("Victor Mono", {weight="Regular", italic=true}),
+	font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true}),
+	color_scheme = "Dracula+",
+}
