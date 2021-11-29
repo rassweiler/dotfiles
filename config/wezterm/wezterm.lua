5d6b4f8fd943284e11ce43a9a999279695188c42
Kyle Rassweiler <kylerassweiler@gmail.com>
2021-11-28 20:34:08 -0500

    Begin looking into neovim configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,4 +3,5 @@ return {
 	font = wezterm.font("Victor Mono", {weight="Regular", italic=true}),
 	font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true}),
 	color_scheme = "Dracula+",
+	window_background_opacity = 0.8,
 }
