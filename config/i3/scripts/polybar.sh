158523ef6acd0fca109d694b40a9214dec53ac20
Kyle Rassweiler <dev@kylerassweiler.ca>
2022-09-04 19:34:43 -0400

    revamp bar styling

--- config/i3/scripts/polybar.sh
+++ config/i3/scripts/polybar.sh
@@ -7,17 +7,15 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "--- Startup ---" | tee -a /tmp/mainbar-i3.log /tmp/secondarybar-i3.log /tmp/dummy.log
+echo "--- Startup ---" | tee -a /tmp/mainbar-i3.log /tmp/secondarybar-i3.log
 outputs=$(polybar --list-monitors | cut -d":" -f1)
 monitors=($outputs)
 primary=${monitors[0]}
 for m in "${monitors[@]}"; do
 	if [ "$m" = "$primary" ]; then
 		MONITOR=$m polybar mainbar-i3 2>&1 | tee -a /tmp/mainbar-i3.log & disown
-		MONITOR=$m polybar dummy 2>&1 | tee -a /tmp/dummy.log & disown
 	else
 		MONITOR=$m polybar secondarybar-i3 2>&1 | tee -a /tmp/secondarybar-i3.log & disown
-		MONITOR=$m polybar dummy 2>&1 | tee -a /tmp/dummy.log & disown
 	fi
 done
 echo "Bars launched..."
