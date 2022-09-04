f2c938a1ae449f30464bf77e682ec280880d786a
Kyle Rassweiler <dev@kylerassweiler.ca>
2022-09-04 08:35:07 -0400

    add polybar multi monitor support

--- config/i3/scripts/polybar.sh
+++ config/i3/scripts/polybar.sh
@@ -8,7 +8,8 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/mainbar-i3.log /tmp/dummy.log
-polybar mainbar-i3 2>&1 | tee -a /tmp/mainbar-i3.log & disown
-polybar dummy 2>&1 | tee -a /tmp/dummy.log & disown
-
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar mainbar-i3 2>&1 | tee -a /tmp/mainbar-i3.log & disown
+	MONITOR=$m polybar dummy 2>&1 | tee -a /tmp/dummy.log & disown
+done
 echo "Bars launched..."
