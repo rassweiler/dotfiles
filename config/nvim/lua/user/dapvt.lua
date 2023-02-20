dbc710c11f2adb8f1a4fe8bcb4ca8784eb6e84b9
Kyle Rassweiler <dev@kylerassweiler.ca>
2023-02-19 19:07:21 -0500

    Refactor neovim configs, use tj devries base

--- config/nvim/lua/user/dapvt.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, dapvt = pcall(require, "dap-virtual-text")
-if not status_ok then
-	return
-end
-
-dapvt.setup()
--- config/nvim/lua/user/fugitive.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, fugitive = pcall(require, "vim-fugitive")
-if not status_ok then
-	return
-end
-
-fugitive.setup()
