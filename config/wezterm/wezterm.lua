e3bef32226f57837cdc211728e19a974d2b2964a
Kyle Rassweiler <dev@kylerassweiler.ca>
2023-02-19 19:06:28 -0500

    Update dotfile configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,4 +3,5 @@ return {
 	font = wezterm.font("JetBrainsMono Nerd Font", {weight="Regular", italic=true}),
 	color_scheme = "Dracula",
 	window_background_opacity = 0.8,
+	scrollback_lines = 3500,
 }
