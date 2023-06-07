d53ff7b1c019c4a00dc854d175ee9074889467b4
Kyle Rassweiler <dev@kylerassweiler.ca>
2023-06-07 07:51:08 -0400

    update font size
    Signed-off-by: Kyle Rassweiler <dev@kylerassweiler.ca>

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm';
 return {
 	font = wezterm.font("JetBrainsMono Nerd Font", {weight="Regular", italic=true}),
+	font_size = 12,
 	color_scheme = "Dracula",
 	window_background_opacity = 0.8,
 	scrollback_lines = 3500,
