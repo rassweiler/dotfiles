f6a4c2f794a61b7c829f527e0d8b42fa84e3b8d4
Kyle Rassweiler <dev@kylerassweiler.ca>
2023-11-01 10:52:22 -0400

    cleanup scripts
    Signed-off-by: Kyle Rassweiler <dev@kylerassweiler.ca>

---
+++ config/swww/set-wallpaper.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#  ----------------------------------------------------- 
+# Select wallpaper
+# ----------------------------------------------------- 
+selected=$(ls -1 ~/Cloud/Photos/Wallpapers | wofi --show drun -IG)
+
+if [ "$selected" ]; then
+
+    echo "Changing theme..."
+    # ----------------------------------------------------- 
+    # Update wallpaper with pywal
+    # ----------------------------------------------------- 
+    wal -q -i ~/Cloud/Photos/Wallpapers/$selected
+
+    # ----------------------------------------------------- 
+    # Get new theme
+    # ----------------------------------------------------- 
+    source "$HOME/.cache/wal/colors.sh"
+
+    # ----------------------------------------------------- 
+    # Copy selected wallpaper into .cache folder
+    # ----------------------------------------------------- 
+    cp $wallpaper ~/.cache/current_wallpaper.jpg   
+
+    newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+
+    # ----------------------------------------------------- 
+    # Set the new wallpaper
+    # ----------------------------------------------------- 
+    swww img $wallpaper \
+        --transition-bezier .43,1.19,1,.4 \
+        --transition-fps=60 \
+        --transition-type="random" \
+        --transition-duration=0.7 \
+        --transition-pos "$( hyprctl cursorpos )"
+
+    ~/.config/waybar/launch.sh
+    sleep 1
+
+    # ----------------------------------------------------- 
+    # Send notification
+    # ----------------------------------------------------- 
+    notify-send "Colors and Wallpaper updated" "with image $newwall"
+
+    echo "Done."
+fi
